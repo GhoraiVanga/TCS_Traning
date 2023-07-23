0faba9bf3d5d76c2a131ded3be6005ed5f525af2
GhoraiVanga <ghorai77@gmail.com>
2023-07-23 14:43:48 +0530

    2nd commit

--- src/test/java/com/example/Test/CalculationTest.java
+++ src/test/java/com/example/Test/CalculationTest.java
@@ -1,7 +1,7 @@
 package com.example.Test;
 import org.junit.jupiter.api.Test;
-import static org.junit.Assert.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class CalculationTest {
--- src/test/java/com/example/demo/demo.java
+++ src/test/java/com/example/demo/demo.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+
 import org.assertj.core.api.Assertions;
+
 import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -9,12 +14,11 @@ import reactor.test.StepVerifier;
 
 import java.util.ArrayList;
 import java.util.Collections;
+
 import java.util.List;
 
-
-
-
 public class demo {
+
 
     Flux<String> source = Flux.just("John", "Monica");
 
@@ -26,10 +30,9 @@ public class demo {
     }
 
     @Test
-      void Test_Field()
-    {
+    void Test_Field() {
         //Create a Flux
-        Flux <Integer> fluxToTest = Flux.just(1, 2, 3, 4, 5);
+        Flux<Integer> fluxToTest = Flux.just(1, 2, 3, 4, 5);
 
         // Create a test
         StepVerifier.create(fluxToTest)
@@ -45,25 +48,24 @@ public class demo {
 
 
     @Test
-     void  test_condition()
-    {
+    void test_condition() {
         Flux<String> error = source.concatWith(
                 Mono.error(new IllegalArgumentException("Our message"))
         );
         StepVerifier.create(error)
 //                .expectNext("John")
 //                .expectNext("Monica")
-                .expectNextCount(1 )
+                .expectNextCount(1)
                 .expectNext("Monica")
                 .expectErrorMessage("Our message")
                 .verify();
     }
 
 
-    public void testExpected()
-    {
+    public void testExpected() {
         throw new IllegalArgumentException();
     }
+
     @Test
     public void discardLocalOrder() {
         List<String> discardOrder = Collections.synchronizedList(new ArrayList<>(2));
@@ -76,11 +78,104 @@ public class demo {
                 .expectComplete()
                 .verify();
         Assertions.assertThat(discardOrder).containsExactly("FIRST", "SECOND");
+        System.out.println("hello");
+    }
+
+    @Test
+    public void testConvertToDoubleOK() {
+        // Test case with the age is a numeric string
+        String age = "1990";
+        Double expAge = Double.valueOf(age);
+        Double actual = StringUtils.convertToDouble(age);
+
+        assertAll("Do many assertions.", () -> {
+            assertNotNull(actual);
+            assertEquals(expAge, actual);
+        });
+
+        // Or Java 8 Lambdas style
+
+        assertAll("Do many assertions.Java 8 Lambdas style", () -> {
+            assertNotNull(actual, () -> "The actual is NULL");
+            assertEquals(expAge, actual,
+                    () -> "The expected is: " + expAge + " while the actual is:" + actual);
+        });
+
     }
 
 
+    @Test
+    public void testConvertToDoubleOKIS_NULL() {
+        String age = null;
+
+        Double actual = StringUtils.convertToDouble(age);
+        assertNull(actual);
+
+
+    }
+
+    @Test
+    public void isNullOrBlank_Test()
+    {
+        String st = null ;
+        String st1 = " ";
+        boolean check = StringUtils.isNullOrBlank(st);
+
+
+        assertAll("Checking ", () -> {
+            assertEquals(Boolean.TRUE ,check);
+
+        });
+    }
+
+ @Test
+    public void checkifitsnotnull()
+ {
+     String st = null ;
+     String defaultSt = "hello";
+
+      String check = StringUtils.getDefaultIfNull(st,defaultSt) ;
+
+
+       assertEquals(defaultSt,check);
+
+ }
+
+ @Test
+    public void checkConcat()
+ {
+     String str1 = "hello";
+     String str2 = "by";
+
+     String actual = str1 +str2 ;
+     String expected = StringUtils.concat(actual);
+
+     assertEquals(actual,expected);
+
+ }
+
+ @Test
+    public void checkConcat_Null_ADD()
+    {
+        String str1 = "hello";
+        String str2 = "by";
+        String str3  = null ;
+
+        String actual = str1 +str2+str3;
+        String expected = StringUtils.concat(actual);
+
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void checkConcatWithNull() {
 
 
 
+        String expected = StringUtils.concat();
+
+        assertNull(expected);
+
+    }
 
 }
